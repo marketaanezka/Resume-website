cde7c3ec85b4820d24a75d08033ee87243c2c11e
Marketa Krucka <marketakrucka@gmail.com>
2020-07-23 22:15:33 +0200

    scroll when clicked on nav item

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 
 console.log("funguju")
 
+
+//scrolls down when clicked on a link from navbar
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
