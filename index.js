e2e8a264ce1b554cabe7ded90c193cde584de316
Marketa Krucka <marketakrucka@gmail.com>
2020-07-25 23:04:26 +0200

    changed skill bars slightly, draft of a function to make active icons on scroll

--- index.js
+++ index.js
@@ -8,9 +8,33 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
         let element = document.querySelector(this.getAttribute('href'));
+        //acounts for the hegiht of the navbar
         let yOffset = -80;
         let y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
         window.scrollTo({top: y, behavior: 'smooth'});
     });
 });
 
+// const navItems = document.querySelectorAll(".navitem"); //returns a NodeList
+//     const navItemsArr = Array.prototype.slice.call(navItems);
+//     console.log(navItemsArr[0].hash);
+
+//shows which part of the page I am on 
+window.onscroll = function() {
+    let scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+       console.log(scrollPosition);
+    const resumeParts = document.querySelectorAll(".part"); //returns a NodeList
+    //creating an array from a nodelist
+    const resumePartsArr = Array.prototype.slice.call(resumeParts);
+       
+    resumePartsArr.forEach(function(item) {
+        let target = item.getBoundingClientRect().y;
+        let id = item.id;
+    
+        if (target <= scrollPosition) {
+            console.log(id);
+            // document.querySelectorAll('.navitem').classList.remove(".active");
+            // document.querySelectorAll('nav > a[href=#' + id + ']').classList.add('active');
+        }
+    });
+} 
