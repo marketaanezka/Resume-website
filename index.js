9d734d82fc02eae511a93f252bc4646c428c7486
Marketa Krucka <marketakrucka@gmail.com>
2020-07-27 20:15:33 +0200

    changes smooth scroll into css, functioning scollspy

--- index.js
+++ index.js
@@ -3,38 +3,39 @@
 console.log("funguju")
 
 
-//scrolls down when clicked on a link from navbar
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        let element = document.querySelector(this.getAttribute('href'));
-        //acounts for the hegiht of the navbar
-        let yOffset = -80;
-        let y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
-        window.scrollTo({top: y, behavior: 'smooth'});
+//scrolls down when clicked on a link from navbar - I used css for this
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+//         let element = document.querySelector(this.getAttribute('href'));
+//         //acounts for the hegiht of the navbar
+//         let yOffset = -80;
+//         let y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+//         window.scrollTo({top: y, behavior: 'smooth'});
+//     });
+// });
+
+
+//shows which part of the page I am on / scrollspy
+let scrollspy = function () {
+    let resumePart = document.querySelectorAll(".part"); //returns a NodeList
+    let resumeParts = {};
+    let i = 0;
+
+    Array.prototype.forEach.call(resumePart, function (e) {
+        resumeParts[e.id] = e.offsetTop - 80;
     });
-});
 
-// const navItems = document.querySelectorAll(".navitem"); //returns a NodeList
-//     const navItemsArr = Array.prototype.slice.call(navItems);
-//     console.log(navItemsArr[0].hash);
+    window.onscroll = function () {
+        let scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
 
-//shows which part of the page I am on 
-window.onscroll = function() {
-    let scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
-       console.log(scrollPosition);
-    const resumeParts = document.querySelectorAll(".part"); //returns a NodeList
-    //creating an array from a nodelist
-    const resumePartsArr = Array.prototype.slice.call(resumeParts);
-       
-    resumePartsArr.forEach(function(item) {
-        let target = item.getBoundingClientRect().y;
-        let id = item.id;
-    
-        if (target <= scrollPosition) {
-            console.log(id);
-            // document.querySelectorAll('.navitem').classList.remove(".active");
-            // document.querySelectorAll('nav > a[href=#' + id + ']').classList.add('active');
+        for (i in resumeParts) {
+            if (resumeParts[i] <= scrollPosition) {
+                document.querySelector('.active').setAttribute('class', 'navitem');
+                document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active', 'navitem');
+            }
         }
-    });
-} 
+    }
+};
+
+scrollspy();
