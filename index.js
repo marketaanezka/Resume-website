5084b6035cf02e039808efa27e89d558b274adee
Marketa Krucka <marketakrucka@gmail.com>
2020-07-29 21:03:44 +0200

    animation on scroll for all skillbars

--- index.js
+++ index.js
@@ -39,57 +39,33 @@ let scrollspy = function () {
 };
 
 scrollspy();
- 
+
 
 //animate skills bars when in view
-(function() {
-    var htmlElement;
-    var windowHeight;
-  
-    function init() {
-      htmlElement = document.querySelector('.no-animation');
-      windowHeight = window.innerHeight;
-    }
-  
-    function checkPosition() {
-    var positionFromTop = htmlElement.getBoundingClientRect().top;
-    if (positionFromTop - windowHeight <= 0) {
-          htmlElement.classList.add('test__html');
-          htmlElement.classList.remove('no-animation');
-        }
-    }
-  
-    window.addEventListener('scroll', checkPosition);
-    window.addEventListener('resize', init);
-  
-    init();
-    checkPosition();
-  })();
 
-  (function() {
+(function () {
     var skillBars;
     var windowHeight;
-  
+
     function init() {
-      skillBars = document.querySelectorAll('.hidden');
-      windowHeight = window.innerHeight;
+        skillBars = document.querySelectorAll('.skill-bar');
+        windowHeight = window.innerHeight;
     }
-  
+
     function checkPosition() {
-      for (var i = 0; i < skillBars.length; i++) {
-        var element = skillBars[i];
-        var positionFromTop = skillBars[i].getBoundingClientRect().top;
-  
-        if (positionFromTop - windowHeight <= 0) {
-          element.classList.add('fade-in-element');
-          element.classList.remove('hidden');
+        for (var i = 0; i < skillBars.length; i++) {
+            var oneBar = skillBars[i];
+            var positionFromTop = skillBars[i].getBoundingClientRect().top;
+
+            if (positionFromTop - windowHeight <= 0) {
+                oneBar.classList.remove('no-animation');
+            }
         }
-      }
     }
-  
+
     window.addEventListener('scroll', checkPosition);
     window.addEventListener('resize', init);
-  
+
     init();
     checkPosition();
-  })();
+})();
